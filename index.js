1182dc2aeb78d8e4aca84d6301687f2f5d8ff727
Radu <paladaradu95@.gmail.com>
2023-08-26 20:09:56 +0300

    fixed sidenav status

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const sideNav = document.querySelector("#sideNav")
 const overlay = document.querySelector("#overlay")
 const mediaQuery = window.matchMedia("(max-width: 768px)")
 
+function closeNav() {
+  sideNav.style.display = "none"
+  overlay.style.display = "none"
+}
+
+
 function openNav() {
   sideNav.style.display = "block"
   overlay.style.display = "block"
 }
 
-function closeNav() {
-  sideNav.style.display = "none"
-  overlay.style.display = "none"
-}
+
